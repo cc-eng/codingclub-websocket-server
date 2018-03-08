acd840b53997856a1c2e75dc2692c1d86502c1a5
Braxton Plaxco <bplaxco@lupnix.org>
2018-03-08 13:06:19 -0500

    Try removing code that doesn't look needed
    ~ B'ezrat Hashem ~

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 console.log("Starting on port 8080");
 
-// Broadcast to all.
-wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
-};
-
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
     console.log("Recieved Message");
