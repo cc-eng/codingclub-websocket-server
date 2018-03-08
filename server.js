3c0edd49fa53c3068e64eb80d491d86e94dba4c5
Braxton Plaxco <bplaxco@lupnix.org>
2018-03-08 15:19:44 -0500

    Require JSON
    ~ B'ezrat Hashem ~

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ console.log("Starting on port 8080");
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
-    if (!data.trim()) return;
+    var msg;
+
+    try {
+      msg = JSON.parse(data):
+    } catch (err) {
+      console.log(err);
+      return;
+    }
 
     console.log("Recieved message " + data);
 
     // Broadcast to everyone else.
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
+        client.send(JSON.stringify(data));
       }
     });
   });
